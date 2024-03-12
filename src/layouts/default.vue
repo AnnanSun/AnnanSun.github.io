46d666621a06c3d583e70825bb1569b55a82e0a7
an.sun <an.sun@boran-tech.com>
2024-03-12 18:03:13 +0800

    feat：change to VUE

---
+++ src/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+	<div
+		font-sans
+		text=" gray-700 dark:gray-200"
+		class="box-border h-screen w-full flex flex-col"
+	>
+		<div class="sticky top-0 z-999"><TheHeader /></div>
+		<div class="box-border w-full flex flex-col ">
+			<!-- <router-view v-if="$route.meta.keepAlive" v-slot="{ Component }">
+				<transition name="move" mode="out-in">
+					<keep-alive>
+						<component :is="Component"></component>
+					</keep-alive>
+				</transition>
+			</router-view> -->
+
+			<!-- <router-view v-if="!$route.meta.keepAlive" v-slot="{ Component }">
+				<transition>
+					<component :is="Component" />
+				</transition>
+			</router-view> -->
+			123
+		</div>
+	</div>
+</template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.25s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+</style>
